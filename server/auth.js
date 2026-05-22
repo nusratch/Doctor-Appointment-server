822b440d600fca1a40f97af5c3cab721bb481bd8
nusrat <nusratchy.002@gmail.com>
2026-05-23 01:01:35 +0600

    fixed url

--- server/auth.js
+++ server/auth.js
@@ -1,23 +1,37 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
 import { betterAuth } from "better-auth";
 
 export const auth = betterAuth({
-  secret: process.env.BETTER_AUTH_SECRET,
+
+  secret:
+    process.env.BETTER_AUTH_SECRET,
 
   baseURL:
-    "https://doctor-appointment-server-4nbo00mez-nusrats-projects-299df817.vercel.app",
+    "https://doctor-appointment-server-seven.vercel.app",
 
   trustedOrigins: [
-    "https://doctor-appointment-client-jby5s6196-nusrats-projects-299df817.vercel.app",
+
+    "https://doctor-appointment-client-psi.vercel.app",
+
+    "http://localhost:5173",
+
   ],
 
   socialProviders: {
+
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
+
+      clientId:
+        process.env.GOOGLE_CLIENT_ID,
+
       clientSecret:
         process.env.GOOGLE_CLIENT_SECRET,
+
     },
+
   },
+
 });
