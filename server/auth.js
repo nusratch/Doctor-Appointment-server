289d8a2c165383b398b6dbdf48e02e0c04bfbb09
nusrat <nusratchy.002@gmail.com>
2026-05-22 20:47:44 +0600

    final cors fix

--- server/auth.js
+++ server/auth.js
@@ -7,21 +7,23 @@ export const auth = betterAuth({
 
   secret: process.env.BETTER_AUTH_SECRET,
 
-baseURL: "https://doctor-appointment-server-seven.vercel.app",
+  baseURL:
+    "https://doctor-appointment-server-seven.vercel.app",
 
-trustedOrigins: [
-  "https://doctor-appointment-client-psi.vercel.app",
-  "https://doctor-appointment-client-jby5s6196-nusrats-projects-299df817.vercel.app",
-],
+  trustedOrigins: [
+    "https://doctor-appointment-client-psi.vercel.app",
+    "https://doctor-appointment-client-jby5s6196-nusrats-projects-299df817.vercel.app",
+  ],
 
   socialProviders: {
 
-    
     google: {
 
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId:
+        process.env.GOOGLE_CLIENT_ID,
 
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret:
+        process.env.GOOGLE_CLIENT_SECRET,
 
     },
 
