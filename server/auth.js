1a751e632638e92790660ee5646f22a66ec2a4bc
nusrat <nusratchy.002@gmail.com>
2026-05-25 02:34:10 +0600

    url  fix

--- server/auth.js
+++ server/auth.js
@@ -17,7 +17,7 @@ const auth =
       process.env.BETTER_AUTH_SECRET,
 
     baseURL:
-      "https://doctor-appointment-server-seven.vercel.app/api/auth",
+      "https://doctor-appointment-server-seven.vercel.app",
 
     trustedOrigins: [
 
@@ -26,6 +26,14 @@ const auth =
       "http://localhost:5173",
 
     ],
+
+    session: {
+
+      cookieCache: {
+        enabled: true,
+      },
+
+    },
 
     database:
       mongodbAdapter(
